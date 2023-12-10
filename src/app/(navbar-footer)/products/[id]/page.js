ae479b55e5b4bfd2e30e287038e3a2b869c688f5
Fahim <siffahim25@gmail.com>
2023-12-10 10:49:47 +0600

    refector: :tada: sererside render

--- src/app/(navbar-footer)/products/[id]/page.js
+++ src/app/(navbar-footer)/products/[id]/page.js
@@ -1,5 +1,7 @@
 const ProductDetail = async ({ params }) => {
-  const res = await fetch(`http://localhost:5000/watches/${params.id}`);
+  const res = await fetch(`http://localhost:5000/watches/${params.id}`, {
+    cache: "no-store",
+  });
   const data = await res.json();
 
   const { image, name, price, features, status, rating } = data;
